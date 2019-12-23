844bbb43f693456063e686e1b1d8800dc558dc14
zhangsan <zansan@qq.com>
2019-12-23 11:33:50 +0800

    测试成功1

---
+++ demo01/src/main/java/com/hhf/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.hhf.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/test")
+    public String test(){
+
+        return "成功";
+    }
+}
